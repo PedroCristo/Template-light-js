57533f0bd7e88fe6dea09957464d4311f9aab2bc
Pedro Cristo <pcristo35@gmail.com>
2022-10-31 14:19:28 +0000

    Add Html structure, css and script

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+let btn = document.getElementById("btn");
+let light = document.getElementById("light");
+let menu = document.getElementById("menu");
+
+function toggleBtn() {
+    btn.classList.toggle("active");
+    light.classList.toggle("active");
+}
